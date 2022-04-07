760920fc77067ca45ec9b7a5b835d738ad8d200c
bayulee <bayuleepaul@gmail.com>
2022-04-07 14:37:53 +0000

    first commit

---
+++ controllers/usersControllers.js
@@ -0,0 +1,27 @@
+const users = require("../data/users")
+
+
+const getUsers=(req, res)=>{
+    res .status(200).json({sucess:true, data:users});
+}
+//create a single user
+const createUser =(req, res)=>{
+    const newUser=req.body;
+    users.push(newUser)
+    res.status(200).json(users)
+}
+//get user by id
+const getUser =(req, res)=>{
+const userId=req.params.id;
+const user=users.find((u)=>u.id==userId);
+
+if(!user){
+    return res.status(400).json({success:false,message:"user not found"});
+}
+res.status(200).json({success:true, data:user});
+}
+
+
+
+
+module.exports ={getUsers, createUser ,getUser}
---
+++ data/users.js
@@ -0,0 +1,14 @@
+const users=[
+    {id:1, name:"john", age:24},
+    {id:2, name:"andrews", age:14},
+    {id:3, name:"roger", age:34},
+    {id:4, name:"mary", age:52},
+    {id:5, name:"rose", age:7},
+    {id:6, name:"joe", age:34},
+    {id:7, name:"bros", age:19},
+    {id:8, name:"roland", age:24},
+    {id:9, name:"maggi", age:42},
+    {id:10, name:"loviea", age:7}
+
+]
+module.exports=users
